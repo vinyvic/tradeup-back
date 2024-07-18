96fe56ccc5d36aad2f5cb7a54526e0a7d68697dd
vinyvic <vinicius518vi@gmail.com>
2024-07-17 21:11:37 -0300

    Validation

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -9,12 +9,26 @@ class apiController extends Controller
 {
     function index(string $cep)
     {
+        if (strlen($cep) != 8) {
+            return response()->json([
+                'ok' => false,
+                'message' => 'O CEP deve ter 8 digitos',
+            ], 422);
+        }
+
         $response = Http::get("https://viacep.com.br/ws/{$cep}/json/");
-        if ($response->ok()){
-            return $response->json();
-        }
-        else {
-            return ['erro' => 'CEP não encontrado'];
-        }
+
+        if ($response->successful()) {
+            $dataFetched = $response->json();
+
+            if (!isset($dataFetched['erro'])) {
+                return $dataFetched;
+            }
+        } 
+
+        return response()->json([
+            'ok' => false,
+            'message' => "O Endereço do CEP $cep não foi encontrado via API",
+        ], 422);
     }
 }
